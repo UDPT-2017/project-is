2d5a924ebe1b35c3c8b83ba7bc26013b3c6ef091
KietHa <hamanhkiet77@gmail.com>
2017-06-08 23:51:11 +0700

    replace by each hbs in homepage

--- app/controllers/homeController.js
+++ app/controllers/homeController.js
@@ -1,7 +1,18 @@
 var homeController = {
   index: function(req, res) {
+
+var images = [
+  {image: "images/image.jpg",
+  tieude: "Tiêu đề 1"},
+  {image: "images/image2.jpg",
+  tieude: "Tiêu đề 2"},
+  {image: "images/image3.jpg",
+  tieude: "Tiêu đề 3"}
+];
+
     res.render('home/index', {
-      title: 'Đặt phòng khách sạn'
+      title: 'Đặt phòng khách sạn',
+      imagesKS: images
     });
   }
 };
