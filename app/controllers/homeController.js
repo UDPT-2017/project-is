35b3b7871ce95d7b25c5a3aefd8e7b2ec8653644
KietHa <hamanhkiet77@gmail.com>
2017-06-10 18:10:06 +0700

    use database and add moment.js

--- app/controllers/homeController.js
+++ app/controllers/homeController.js
@@ -1,18 +1,12 @@
+var Image = require("../models/image");
+
 var homeController = {
   index: function(req, res) {
-
-var images = [
-  {image: "images/image.jpg",
-  tieude: "Tiêu đề 1"},
-  {image: "images/image2.jpg",
-  tieude: "Tiêu đề 2"},
-  {image: "images/image3.jpg",
-  tieude: "Tiêu đề 3"}
-];
-
-    res.render('home/index', {
-      title: 'Đặt phòng khách sạn',
-      imagesKS: images
+    Image.getAll(function(err, images){
+      res.render('home/index', {
+        title: 'Đặt phòng khách sạn',
+        imagesKS: images
+      });
     });
   }
 };
---
+++ app/models/image.js
@@ -0,0 +1,33 @@
+var pg = require('pg');
+
+// var connectionString = "postgres://postgres:123@localhost:5432/photos";
+
+var config = {
+  user: 'postgres', //env var: PGUSER
+  database: 'photos', //env var: PGDATABASE
+  password: '123', //env var: PGPASSWORD
+  host: 'localhost', // Server hosting the postgres database
+  port: 5432, //env var: PGPORT
+  max: 10, // max number of clients in the pool
+  idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
+};
+
+var pool = new pg.Pool(config);
+
+var Image = {
+  getAll: function(callback) {
+    // var client = new pg.Client(connectionString);
+    // pool.connect(function(err, client, done) {
+    //   if (err) throw err;
+
+    // setTimeout(function() {
+    pool.query("select url, name from photo", function(err, result) {
+      // done(err);
+      callback(err, result.rows);
+    });
+    // }, 5000);
+    // });
+  }
+};
+
+module.exports = Image;
