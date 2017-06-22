639bf32730206cca8d1ffb8889a6276ed38f4737
tqhb1995 <1412553@student.hcmus.edu.vn>
2017-06-22 17:12:39 +0700

    Update Chat

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,53 @@
+var socket = io("http://localhost:3000");
+
+
+socket.on("server-send-dki-thanhcong", function(data){
+  $("#currentUser").html(data);
+  $("#loginForm").hide(2000);
+  $("#chatForm").show(1000);
+});
+
+socket.on("server-send-mesage", function(data){
+  $("#listMessages").append("<div class='ms'>" + data.un + ":" + data.nd +"</div>");
+});
+
+socket.on("enterMess", function(data){
+  $("#thongbao").html("<img width='20px' src='image/waitMess.gif'> " + data);
+});
+
+socket.on("stopEnterMess", function(){
+  $("#thongbao").html("");
+});
+
+
+$(document).ready(function(){
+  $("#loginForm").show();
+  $("#chatForm").hide();
+
+  $("#txtMessage").focusin(function(){
+    socket.emit("myEnterMess");
+  })
+
+  $("#txtMessage").focusout(function(){
+    socket.emit("myStopMess");
+  })
+
+  $("#btnRegister").click(function(){
+    socket.emit("client-send-Username", $("#txtUsername").val());
+  });
+
+
+  $("#btnSendMessage").click(function(){
+    socket.emit("user-send-message", $("#txtMessage").val());
+  });
+
+
+});
+
+
+
+
+
 (function($) {
     $(document).ready(function() {
         var $chatbox = $('.chatbox'),
@@ -20,5 +70,3 @@
         });
     });
 })(jQuery);
-
-
