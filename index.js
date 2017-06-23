cb61cf80df497e0fc6fed3cb52f98993a9e25af8
tqhb1995 <1412553@student.hcmus.edu.vn>
2017-06-23 15:37:58 +0700

    update

--- index.js
+++ index.js
@@ -1,48 +1,10 @@
 const express = require('express');
 const app = express();
-var server = require("http").Server(app);
-var io = require("socket.io")(server);
 
 require('./config')(app);
 
 app.listen(3000, function() {
   console.log('Example app listening on port 3000!');
-});
-
-
-
-
-
-
-var mangUsers=[];
-
-io.on("connection", function(socket){
-  console.log("Co nguoi ket noi " + socket.id);
-
-  socket.on("client-send-Username", function(data){
-    if(mangUsers.indexOf(data)>=0){
-      socket.emit("server-send-dki-thatbai");
-    }else{
-      mangUsers.push(data);
-      socket.Username = data;
-      socket.emit("server-send-dki-thanhcong", data);
-    }
-  });
-
-  socket.on("user-send-message", function(data){
-    io.sockets.emit("server-send-mesage", {un:socket.Username, nd:data} );
-  });
-
-  socket.on("myEnterMess", function(){
-    var s = socket.Username + " đang trả lời";
-    io.sockets.emit("enterMess", s);
-  });
-
-  socket.on("myStopMess", function(){
-    io.sockets.emit("stopEnterMess");
-  });
-
-
 });
 
 
